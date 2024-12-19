a348f48010d92c0ddb582ec37f47638ae44ef1cf
Brian Haas <bhaas@broadinstitute.org>
2024-12-19 00:33:17 +0000

    enforce list type on input param

--- pylib/EM_ChatGPT_based.py
+++ pylib/EM_ChatGPT_based.py
@@ -11,7 +11,10 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def run_EM(transcripts, max_EM_iterations=1000):
+def run_EM(transcripts: list, max_EM_iterations: int = 1000):
+
+    if type(transcripts) != list:
+        transcripts = list(transcripts)
 
     local_debug = False
 
