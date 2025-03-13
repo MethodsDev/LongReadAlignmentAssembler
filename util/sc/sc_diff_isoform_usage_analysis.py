85d953f6baabfa49b15a9834c7dcbc408568f490
Brian Haas <brianjohnhaas@gmail.com>
2025-03-13 06:44:04 -0400

    generate tpm and count matrices for pseudobulked clusters

--- util/sc/sc_diff_isoform_usage_analysis.py
+++ util/sc/sc_diff_isoform_usage_analysis.py
@@ -57,6 +57,13 @@ def main():
     )
 
     parser.add_argument(
+        "--output_prefix",
+        type=str,
+        required=True,
+        help="prefix for output files",
+    )
+
+    parser.add_argument(
         "--run_demo_test",
         action="store_true",
         default=False,
@@ -74,10 +81,25 @@ def main():
     sc_cluster_expr_dir = args.sc_cluster_expr_dir
     collapse_splice_identical = args.collapse_splice_identical
     ignore_unspliced = args.ignore_unspliced
+    output_prefix = args.output_prefix
 
-    cluster_names, big_df = build_big_sc_cluster_count_df(
+    cluster_names, counts_big_df, TPM_big_df = build_big_sc_cluster_count_df(
         sc_cluster_expr_dir, collapse_splice_identical, ignore_unspliced
     )
+
+    counts_big_df_fname_prefix = f"{output_prefix}.full.counts.tsv"
+    TPM_big_df_fname_prefix = f"{output_prefix}.full.TPM.tsv"
+
+    if ignore_unspliced:
+        counts_big_df_fname_prefix += ".ignore_unspliced"
+        TPM_big_df_fname_prefix += ".ignore_unspliced"
+
+    if collapse_splice_identical:
+        counts_big_df_fname_prefix += ".collapse_splice_identical"
+        TPM_big_df_fname_prefix += ".collapse_splice_identical"
+
+    counts_big_df.to_csv(f"{counts_big_df_fname_prefix}.tsv", sep="\t", index=False)
+    TPM_big_df.to_csv(f"{TPM_big_df_fname_prefix}.tsv", sep="\t", index=False)
 
     ## pairwise compare clusters.
 
@@ -88,7 +110,9 @@ def main():
             cluster_j = cluster_names[j]
 
             logger.info("Testing pair: {} vs {}".format(cluster_i, cluster_j))
-            test_df = big_df[["gene_id", "transcript_id", cluster_i, cluster_j]].copy()
+            test_df = counts_big_df[
+                ["gene_id", "transcript_id", cluster_i, cluster_j]
+            ].copy()
             test_df.rename(
                 columns={cluster_i: "count_A", cluster_j: "count_B"}, inplace=True
             )
@@ -107,7 +131,9 @@ def main():
 
                 # all_test_results = pd.concat([all_test_results, test_df_results])
                 test_df_results.to_csv(
-                    f"{cluster_i}-vs-{cluster_j}.diff_iso.tsv", sep="\t", index=False
+                    f"{output_prefix}.{cluster_i}-vs-{cluster_j}.diff_iso.tsv",
+                    sep="\t",
+                    index=False,
                 )
 
     # print(all_test_results)
@@ -219,14 +245,17 @@ def generate_test_data(num_genes=20):
 def build_big_sc_cluster_count_df(
     sc_cluster_expr_dir, collapse_splice_identical=False, ignore_unspliced=False
 ):
-    files = glob.glob(f"{sc_cluster_expr_dir}/*quant.expr")
-    data_frames = []
+    files = glob.glob(f"{sc_cluster_expr_dir}/*quant.expr*")
+
+    count_data_frames = []
+    TPM_data_frames = []
 
     sample_names = list()
 
     for file in files:
-        sample_name = os.path.basename(file).replace(".quant.expr", "")
-        logger.info("-incorporating {} into big count df".format(sample_name))
+        sample_name = os.path.basename(file)
+        sample_name = re.sub("\\.quant.expr.*$", "", sample_name)
+        logger.info("-incorporating {} into count and TPM matrices".format(sample_name))
         df = pd.read_csv(file, sep="\t")  # Adjust separator if necessary
 
         if ignore_unspliced:
@@ -235,20 +264,33 @@ def build_big_sc_cluster_count_df(
         if collapse_splice_identical:
             df = collapse_splice_identical_isoforms(df)
 
-        df = df[["gene_id", "transcript_id", "all_reads"]]
+        df = df[["gene_id", "transcript_id", "all_reads", "TPM"]]
 
-        df.rename(columns={"all_reads": sample_name}, inplace=True)
-        data_frames.append(df)
+        df_counts = df[["gene_id", "transcript_id", "all_reads"]].rename(
+            columns={"all_reads": sample_name}
+        )
+        df_TPM = df[["gene_id", "transcript_id", "TPM"]].rename(
+            columns={"TPM": sample_name}
+        )
+
+        count_data_frames.append(df_counts)
+        TPM_data_frames.append(df_TPM)
         sample_names.append(sample_name)
 
     # Merge all dataframes on gene_id and transcript_id
-    merged_df = data_frames[0]
-    for df in data_frames[1:]:
-        merged_df = pd.merge(
-            merged_df, df, on=["gene_id", "transcript_id"], how="outer"
+    merged_counts_df = count_data_frames[0]
+    for df in count_data_frames[1:]:
+        merged_counts_df = pd.merge(
+            merged_counts_df, df, on=["gene_id", "transcript_id"], how="outer"
         )
 
-    return sample_names, merged_df
+    merged_TPM_df = TPM_data_frames[0]
+    for df in TPM_data_frames[1:]:
+        merged_TPM_df = pd.merge(
+            merged_TPM_df, df, on=["gene_id", "transcript_id"], how="outer"
+        )
+
+    return sample_names, merged_counts_df, merged_TPM_df
 
 
 def collapse_splice_identical_isoforms(df):
@@ -261,12 +303,13 @@ def collapse_splice_identical_isoforms(df):
     def process_group(group_df):
         group_key = group_df.name
         total_reads = group_df["all_reads"].sum()
+        total_TP = group_df["TPM"].sum()
         first_row = group_df.iloc[
             0
         ].copy()  # Get a copy of the first row to preserve other columns.
         first_row["transcript_id"] = group_key
         first_row["all_reads"] = total_reads
-        return first_row[["gene_id", "transcript_id", "all_reads"]]
+        return first_row[["gene_id", "transcript_id", "all_reads", "TPM"]]
 
     result_df = df.groupby("group_key").apply(process_group).reset_index(drop=True)
 
