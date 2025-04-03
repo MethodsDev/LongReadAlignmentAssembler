f580c0bd324ce362be1a5ade3945e1714467057d
Brian Haas <bhaas@broadinstitute.org>
2025-04-03 18:38:34 +0000

    now properly working

--- util/annotate_bam_with_read_tracking_info.py
+++ util/annotate_bam_with_read_tracking_info.py
@@ -6,6 +6,7 @@ import lmdb
 import argparse
 import csv
 import logging
+import shutil
 
 FORMAT = (
     "%(asctime)-15s %(levelname)s %(module)s.%(name)s.%(funcName)s:\n\t%(message)s\n"
@@ -33,6 +34,12 @@ def main():
 
     quant_read_tracking_lmdb_filename = tracking_file + ".lmdb"
 
+    if os.path.exists(quant_read_tracking_lmdb_filename):
+        logger.warning(
+            f"{quant_read_tracking_lmdb_filename} exists already and will be removed and rebuilt now."
+        )
+        shutil.rmtree(quant_read_tracking_lmdb_filename)
+
     build_read_tracking_lmdb(tracking_file, quant_read_tracking_lmdb_filename)
 
     output_bam_file = bam_file + ".tagged.bam"
@@ -47,46 +54,50 @@ def build_read_tracking_lmdb(tracking_file, quant_read_tracking_lmdb_filename):
 
     logger.info("Building and populating read tracking db")
 
-    ofh_quant_read_tracking_lmdb = lmdb.open(
+    env = lmdb.open(
         quant_read_tracking_lmdb_filename,
         map_size=int(1e11),
-        metasync=False,
-        sync=False,
+        # metasync=False,
+        # sync=False,
     )
 
-    txn = ofh_quant_read_tracking_lmdb.begin(write=True)
+    with env.begin(write=True) as txn:
 
-    record_counter = 0
-    with open(tracking_file, "rt") as fh:
+        record_counter = 0
+        with open(tracking_file, "rt") as fh:
 
-        reader = csv.DictReader(fh, delimiter="\t")
-        for row in reader:
+            reader = csv.DictReader(fh, delimiter="\t")
+            for row in reader:
 
-            record_counter += 1
-            if record_counter % 1000 == 0:
-                print(f"\r[{record_counter}] ", file=sys.stderr, end="")
+                record_counter += 1
+                if record_counter % 1000 == 0:
+                    print(f"\r[{record_counter}] ", file=sys.stderr, end="")
 
-            gene_id = row["gene_id"]
-            transcript_id = row["transcript_id"]
-            read_name = row["read_name"]
-            frac_read_assigned = row["frac_assigned"]
-            key = read_name.encode("utf-8")
-            existing_data = txn.get(key)
-            lmdb_data = [
-                gene_id,
-                transcript_id,
-                frac_read_assigned,
-            ]
-            if existing_data:
-                # Append new data as comma-separated string
-                existing_data = existing_data.decode("utf-8")
-                combined_data = f"{existing_data},{','.join(lmdb_data)}"
-            else:
-                combined_data = ",".join(lmdb_data)
+                gene_id = row["gene_id"]
+                transcript_id = row["transcript_id"]
+                read_name = row["read_name"]
+                frac_read_assigned = row["frac_assigned"]
+                key = read_name.encode("utf-8")
+                existing_data = txn.get(key)
+                lmdb_data = [
+                    gene_id,
+                    transcript_id,
+                    frac_read_assigned,
+                ]
+                if existing_data:
+                    # Append new data as comma-separated string
+                    existing_data = existing_data.decode("utf-8")
+                    combined_data = f"{existing_data},{','.join(lmdb_data)}"
+                    # print("Storing APPENDED : {} -> {}".format(key, combined_data))
+                else:
+                    combined_data = ",".join(lmdb_data)
+                    # print("Storing init : {} -> {}".format(key, combined_data))
 
-            txn.put(key, combined_data.encode("utf-8"))
+                txn.put(key, combined_data.encode("utf-8"))
 
-    ofh_quant_read_tracking_lmdb.close()
+                # get_val = txn.get(key)
+                # print("retrieved val: {}".format(get_val.decode()))
+
     logger.info("done building read tracker db.")
 
     return
@@ -110,6 +121,7 @@ def annotate_bam_with_read_tracking_info(
     ) as bam_out:
 
         read_counter = 0
+        num_reads_annotated = 0
 
         for read in bam_in.fetch(until_eof=True):
 
@@ -117,7 +129,8 @@ def annotate_bam_with_read_tracking_info(
             if read_counter % 1000 == 0:
                 print(f"\r[{read_counter}] ", file=sys.stderr, end="")
 
-            data = txn.get(read.query_name.encode("utf-8"))
+            read_name = read.query_name
+            data = txn.get(read_name.encode("utf-8"))
             if data:
                 row = data.decode("utf-8").split(",")
                 if read.has_tag("XG") or read.has_tag("XI"):
@@ -132,10 +145,22 @@ def annotate_bam_with_read_tracking_info(
                 read.set_tag("XG", gene_ids, "Z")
                 read.set_tag("XI", transcript_ids, "Z")
                 read.set_tag("XF", frac_assigned, "Z")
+                num_reads_annotated += 1
 
             bam_out.write(read)
 
+        logger.info(
+            "Number of reads assigned transcript info: {}".format(num_reads_annotated)
+        )
+
+        if num_reads_annotated == 0:
+            raise RuntimeError(
+                "Error, no reads were annotated... check the tracking and bam files match up"
+            )
+
     ofh_quant_read_tracking_lmdb.close()
+
+    return
 
 
 if __name__ == "__main__":
