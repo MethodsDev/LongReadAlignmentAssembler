3120ea1eef19c2454ec02d65fd284f8174471487
Brian Haas <bhaas@broadinstitute.org>
2025-05-31 21:29:33 +0000

    use LRAA docker instead of samtools docker

--- Docker/build_docker.sh
+++ Docker/build_docker.sh
@@ -4,10 +4,17 @@ set -ex
 
 VERSION=`cat VERSION.txt`
 
-docker build -t lraa/lraa:${VERSION} .
-docker build -t lraa/lraa:latest .
-docker build -t  us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION} .
-docker build -t  us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:latest .
+#docker buildx create --name terra-builder --use
+
+docker buildx build --platform linux/amd64 \
+  -t us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION} \
+  --push .
+
+
+docker buildx build --platform linux/amd64 \
+  -t us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:latest \
+  --push .
+
 
 # verify
 docker run --rm -it us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:latest /usr/local/src/LRAA/LRAA --version
