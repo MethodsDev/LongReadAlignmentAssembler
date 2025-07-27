7d0324f68202b27022a73530dcef9386ce2afc72
Brian Haas <bhaas@broadinstitute.org>
2025-07-27 13:10:00 +0000

    built docker version 0.7.0-postdev1

--- Docker/build_docker.versioned.sh
+++ Docker/build_docker.versioned.sh
@@ -6,10 +6,13 @@ VERSION=`cat VERSION.txt`
 
 #docker buildx create --name terra-builder --use
 
-docker buildx build --platform linux/amd64 \
-  -t us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION} \
-  --push .
+#docker buildx build --platform linux/amd64 \
+#  -t us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION} \
+#  --push .
 
+
+docker build -t us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION} .
+docker push  us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION}
 
 # verify
 docker run --rm -it us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION} /usr/local/src/LRAA/LRAA --version
