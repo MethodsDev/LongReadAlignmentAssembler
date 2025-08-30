9b579e4a8b5c82da9f6daaa5ccaf8cd032272ef2
Brian Haas <brianjohnhaas@gmail.com>
2025-08-30 11:28:40 -0400

    adding SIRV4-only test data

---
+++ testing/sirvs/SIRV4-only/run_LRAA.sh
@@ -0,0 +1,1 @@
+../../../LRAA --genome SIRV4.genome.fa --bam SIRV4.mm2.bam -d --no_norm
---
+++ testing/sirvs/SIRV4-only/run_igv.sh
@@ -0,0 +1,1 @@
+igv.sh -g SIRV4.genome.fa SIRV4.mm2.bam,SIRV4.annot.gtf,LRAA.gtf.!.gtf,LRAA.gtf.c.gtf,LRAA.gtf.=.gtf,__final_graph.exons.bed,__final_graph.introns.bed,__TSS_info.bed,__PolyAsite_info.bed  SIRV4
