226525f848c23ccf63d50d3eefc45379bf9d13fc
Brian Haas <bhaas@broadinstitute.org>
2025-09-03 17:15:20 +0000

    update docker builder - v0.8.0

--- Docker/build_docker.latest.sh
+++ Docker/build_docker.latest.sh
@@ -2,13 +2,21 @@
 
 set -ex
 
+
+VERSION=latest
+
+
 #docker buildx create --name terra-builder --use
 
-docker buildx build --platform linux/amd64 \
-  -t us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:latest \
-  --push .
+#docker buildx build --platform linux/amd64 \
+#  -t us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:latest \
+#  --push .
 
+
+docker build -t us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION} .
+docker push  us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION}
 
 # verify
-docker pull us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:latest
-docker run --rm -it us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:latest /usr/local/src/LRAA/LRAA --version
+docker run --rm -it us-central1-docker.pkg.dev/methods-dev-lab/lraa/lraa:${VERSION} /usr/local/src/LRAA/LRAA --version
+
+
